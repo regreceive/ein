53db11d68367908364674a725b5dc43d74081c25
liyao <regreceive@163.com>
2018-09-06 14:08:54 +0800

    城市效果

--- src/pages/Home/Banner/effects/Spot.js
+++ src/pages/Home/Banner/effects/Spot.js
@@ -12,20 +12,18 @@ export default class extends React.Component {
     el.style.WebkitMaskPosition = `${clientX - radius}px ${clientY -
       radius +
       this.offsetY}px`;
-    el.style.maskPosition = `${clientX - this.maskSize / 2}px ${clientY -
-      this.maskSize / 2}px`;
+    el.style.maskPosition = `${clientX - radius}px ${clientY - radius}px`;
   }, 100);
 
   offsetY = 0;
   viewWidth = 0;
   viewHeight = 0;
-  maskSize = 0;
+  maskSize = 1125;
 
   resizeThrottled = throttle(() => {
     this.viewWidth = this.ref.current.offsetWidth;
     this.viewHeight = this.ref.current.offsetHeight;
     this.maskSize = (this.viewWidth * 690) / 1920;
-
     this.ref.current.style.WebkitMaskSize = `${this.maskSize}px ${
       this.maskSize
     }px`;
@@ -38,16 +36,16 @@ export default class extends React.Component {
 
   componentDidMount() {
     this.ref.current.addEventListener('mousemove', this.throttled);
-    window.addEventListener('resize', this.resizeThrottled);
+    // window.addEventListener('resize', this.resizeThrottled);
     window.addEventListener('scroll', this.scrollThrottled);
-    this.resizeThrottled();
+    // this.resizeThrottled();
   }
 
   componentDidUpdate() {}
 
   componentWillUnmount() {
     this.ref.current.removeEventListener('mousemove', this.throttled);
-    window.removeEventListener('resize', this.resizeThrottled);
+    // window.removeEventListener('resize', this.resizeThrottled);
     window.removeEventListener('scroll', this.scrollThrottled);
   }
 
