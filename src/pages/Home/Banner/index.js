ec255c0538a354fe610031674675e18a923352b2
YueChengCheng <13834192367@163.com>
2018-09-06 19:01:27 +0800

    文字效果

--- src/pages/Home/Banner/index.js
+++ src/pages/Home/Banner/index.js
@@ -6,22 +6,48 @@ import Progress from '../Progress';
 import Spot from './effects/Spot';
 const Banner = props => {
   const { translate } = props;
+  let result = translate('home.text-1');
+  let ycc = null;
+  let ycc1 = null;
+  if (result.indexOf('Missing translationId') == -1) {
+    ycc = result;
+    ycc1 = ycc.split('');
+  }
 
+  if (ycc1 == null) {
+    return false;
+  }
   return (
     <div styleName="root">
       <Spot />
       <div styleName="container">
         <div styleName="bg-font">{translate('home.soon')}</div>
         <div styleName="Latest-Testnet-Trial">
-          <h4 styleName="text-style-1">{translate('home.text-1')}</h4>
+          <h4 styleName="text-style-1">
+            {ycc1.map((item, i) => (
+              <span
+                styleName="aniSpan"
+                style={{
+                  animationDelay: i / 60 + 's',
+                }}
+                key={i}
+              >
+                {/\s/g.test(item) ? (
+                  <div style={{ width: '16px', height: '3px' }} />
+                ) : (
+                  item
+                )}
+              </span>
+            ))}
+          </h4>
         </div>
         <div styleName="No-of-Shards-256-N">
-          <ul>
+          <ul styleName="company_name">
             <li>{translate('home.li-1')}</li>
             <li>{translate('home.li-2')}</li>
             <li>{translate('home.li-3')}</li>
           </ul>
-          <p>{translate('home.natework')}</p>
+          <p styleName="company_name">{translate('home.natework')}</p>
         </div>
         <div styleName="Rectangle">
           <input placeholder="Email" type="text" />
@@ -30,9 +56,7 @@ const Banner = props => {
         </div>
         <div styleName="mobile-Rectangle">
           <input />
-          <div styleName="btn">
-            <span styleName="arrow" />
-          </div>
+          <div styleName="btn" />
         </div>
       </div>
       <Progress />
