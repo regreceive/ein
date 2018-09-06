df9bf5e5da5ccd00415ee9680aa0538d4c48410a
gechunyue <gechunyue@echoex.io>
2018-09-06 18:12:27 +0800

    线条字体更改

--- src/pages/Home/Progress/index.js
+++ src/pages/Home/Progress/index.js
@@ -14,7 +14,10 @@ export default class Progress extends React.Component {
         <div styleName="line3" />
         <div styleName="step3" />
         <div styleName="line4" />
-        <div styleName="s1TxtTop">3,000+ tx/s</div>
+        <div styleName="s1TxtTop">
+          <span>3,</span>
+          <span>000+ tx/s</span>
+        </div>
         {/* <div styleName="s1TxtBot">I love web</div> */}
         <div styleName="s2TxtTop">5,000+ tx/s</div>
         <div styleName="s3TxtTop">10,000+ tx/s</div>
