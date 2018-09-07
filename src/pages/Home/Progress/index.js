f01e33f245f12e3d7de8916396818a9653b221b9
gechunyue <gechunyue@echoex.io>
2018-09-07 11:22:35 +0800

    线条动画效果更改

--- src/pages/Home/Progress/index.js
+++ src/pages/Home/Progress/index.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import './Progress.css';
+import s from './Progress.css';
 
 export default class Progress extends React.Component {
-  componentDidMount = () => {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      isover: false,
+    };
+  }
+  componentDidMount = () => {
+    var step1 = document.getElementById('step1');
+    step1.addEventListener('webkitAnimationEnd', () => {
+      this.setState({
+        isover: true,
+      });
+    });
+  };
 
   render() {
+    const { isover } = this.state;
     return (
       <div styleName="Progress-box">
+        <div styleName="line" />
         <div styleName="line1" />
-        <div styleName="step1" />
-        <div styleName="line2" />
+        <div
+          className={isover === false ? s.animation1 : s.animation2}
+          id="step1"
+        />
         <div styleName="step2" />
-        <div styleName="line3" />
         <div styleName="step3" />
-        <div styleName="line4" />
         <div styleName="s1TxtTop">
-          <span>3,</span>
-          <span>000+ tx/s</span>
+          <span>3,000+ tx/s</span>
         </div>
         {/* <div styleName="s1TxtBot">I love web</div> */}
         <div styleName="s2TxtTop">5,000+ tx/s</div>
