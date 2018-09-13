b8c5ad5eb82d971e64c7908d91ea6d13d7189d46
liyao <regreceive@163.com>
2018-09-13 19:04:42 +0800

    订阅功能

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -70,11 +70,13 @@ module.exports = {
   // You can exclude the *.map files from the build during deployment.
   devtool: shouldUseSourceMap ? 'source-map' : false,
   // In production, we only want to load the polyfills and the app code.
-  entry: [
-    require.resolve('./polyfills'),
-    require.resolve('whatwg-fetch'),
-    paths.appIndexJs,
-  ],
+  entry: {
+    client: [
+      require.resolve('./polyfills'),
+      require.resolve('whatwg-fetch'),
+      paths.appIndexJs,
+    ],
+  },
   output: {
     // The build folder.
     path: paths.appBuild,
